a3f519962a9ab369aedaed184edb846996d705b0
Anthony <pokepics@hotmail.com>
2020-07-15 18:30:47 -0700

    got boilerplate for server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path")
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+let note = []
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
